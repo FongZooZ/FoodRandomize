e1e2007cc429bc1b73fac9d82a669f4245e150ec
FongZooZ <FongZooZ1@gmail.com>
2015-09-20 22:31:55 +0700

    added some function in controller, fixed view

--- public/javascripts/angular/NameController.js
+++ public/javascripts/angular/NameController.js
@@ -1,12 +1,18 @@
 angular.module('name', ['ngRoute'])
-	.service('NameService', function($http) {
+	.service('NameService', function($http, $rootScope) {
 		this.addName = function(name) {
 			return $http.post('/api/name', name).success(function(response) {
-				// after add done
+				$rootScope.$broadcast('name-added', response);
 			});
-		}
+		};
+		this.getAllName = function() {
+			return $http.get('/api/name').success(function(response) {
+
+			});
+		};
 	})
 	.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+		// don't know why it doesn't work
 		// $routeProvider
 		// 	.when('/nameDisplay', {
 		// 		controller: 'NameController as NameCtrl'
@@ -17,15 +23,37 @@ angular.module('name', ['ngRoute'])
 			requireBase: false
 		});
 	}])
-	.controller('NameController', function(NameService) {
+	.controller('NameController', function($rootScope, NameService) {
 		var self = this;
+		self.names = [];
+		self.init = function() {
+			NameService.getAllName().then(function(result) {
+				self.names = result.data;
+			});
+		}
 		self.addName = function() {
 			var nameField = {
 				display: self.name.display,
 				detail: self.name.detail
 			}
 			NameService.addName(nameField);
-			self.name = {display: '', detail: ''};
-			nameField = {display: '', detail: ''};
+			self.name = {
+				display: '',
+				detail: ''
+			};
+			nameField = {
+				display: '',
+				detail: ''
+			};
 		}
+		self.updateName = function() {
+			NameService.getAllName().then(function(result) {
+				self.names = result.data;
+			});
+		}
+
+		// catch events
+		$rootScope.$on('name-added', function(evt, response) {
+			self.updateName();
+		})
 	});
--- public/javascripts/misc.js
+++ public/javascripts/misc.js
@@ -1,15 +1,3 @@
 (function() {
-	// $('#modal-addName').on('hidden.bs.modal', function(e) {
-	// 	$(this)
-	// 		.find("input,textarea,select")
-	// 		.val('')
-	// 		.end()
-	// 		.find("input[type=checkbox], input[type=radio]")
-	// 		.prop("checked", "")
-	// 		.end();
-	// });
-	// $('#form-addName').on('submit', function() {
-	// 	$('#modal-addName').modal('hide');
-	// 	alert();
-	// });
+
 })();
